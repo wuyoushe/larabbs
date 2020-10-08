d1c82df44effcb94565bcc9ef0c1407ae5cc74ed
wuyoushe <wudadongfen@126.com>
2020-10-08 15:19:32 +0800

    假数据填充

---
+++ database/seeds/TopicsTableSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Topic;
+use App\Models\User;
+use App\Models\Category;
+
+class TopicsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        factory(Topic::class)->times(100)->create();
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = factory(User::class)->times(10)->create();
+        //单独处理第一个用户数据
+        $user = User::find(1);
+        $user->name = 'Felton';
+        $user->email = 'felton@example.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
+        $user->save();
+    }
+}
+
+
+
+
+
+
+
+
+
