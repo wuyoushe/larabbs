8b18bce4a21a407472300c7eb1b43c762a6f1f94
wuyoushe <wudadongfen@126.com>
2020-10-12 20:52:20 +0800

    帖子列表

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,14 +2,14 @@
 
 namespace App\Policies;
 
-use App\Models\User;
+use App\Models\User as ModelUser;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class UserPolicy
 {
     use HandlesAuthorization;
 
-    public function update(User $currentUser, User $user)
+    public function update(ModelUser $currentUser, ModelUser $user)
     {
         return $currentUser->id === $user->id;
     }
--- app/helpers.php
+++ app/helpers.php
@@ -19,6 +19,13 @@ function category_nav_active($category_id)
     return active_class(if_route('categories.show') && if_route_param('category', $category_id));
 }
 
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    //return Str::limit($excerpt, $length);
+    return \Illuminate\Support\Str::limit($excerpt, $length);
+}
+
 
 
 
