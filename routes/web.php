6388868930885c1bcad442c12136d74c865f4770
felton <wudadongfen@126.com>
2020-10-20 23:06:01 +0800

    '用户可以发表评论'

--- routes/web.php
+++ routes/web.php
@@ -48,14 +48,8 @@ Route::resource('categories', 'CategoriesController', ['only' =>['show']]);
 
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
-
-
-
-
-
-
-
-
+// Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destory']]);
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Auth::routes();
@@ -70,4 +64,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+// Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
