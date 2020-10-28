ae31819a336d5348ae6a1302595b1ac9ba17fa28
felton <wudadongfen@126.com>
2020-10-28 22:54:04 +0800

    '站点权限部署'

--- resources/views/topics/_reply_list.blade.php
+++ resources/views/topics/_reply_list.blade.php
@@ -1,6 +1,6 @@
 <ul class="list-unstyled">
   @foreach ($replies as $index => $reply)
-    <li class=" media" name="reply{{ $reply->id }}" id="reply{{ $reply->id }}">
+    <li class="media" name="reply{{ $reply->id }}" id="reply{{ $reply->id }}">
       <div class="media-left">
         <a href="{{ route('users.show', [$reply->user_id]) }}">
           <img class="media-object img-thumbnail mr-3" alt="{{ $reply->user->name }}" src="{{ $reply->user->avatar }}" style="width:48px;height:48px;" />
