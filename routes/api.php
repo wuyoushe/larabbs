5164bfc0e9533fc3510a900989a3ad8c5a1ae129
felton <wudadongfen@126.com>
2020-10-28 18:36:46 +0800

    '添加自动刷新token的中间件'

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,23 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::namespace('Api')->prefix('v1')->middleware('cors')->group(function() {
-    Route::get('/users','UserController@index')->name('users.index');
-    Route::get('/users/{user}','UserController@show')->name('users.show');
+    //用户注册
     Route::post('/users', 'UserController@store')->name('users.store');
+    //用户登录
     Route::post('/login', 'UserController@login')->name('users.login');
-    Route::get('/users/info','UserController@info')->name('users.info');
+
+    Route::middleware('api.refresh')->group(function(){
+        //当前用户信息
+        Route::get('/users/info','UserController@info')->name('users.info');
+        //用户列表
+        Route::get('/users','UserController@index')->name('users.index');
+        //用户信息
+        Route::get('/users/{user}','UserController@show')->name('users.show');
+        //用户退出
+        Route::get('/logout', 'UserController@logout')->name('users.logout');
+    });
+
+
+
+
 });
