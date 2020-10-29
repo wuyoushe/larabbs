b22ccb0cbefdc0350fb906004def016ca26098db
felton <wudadongfen@126.com>
2020-10-29 11:22:47 +0800

    '添加边栏活跃用户'

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category, Request $request, Topic $topic)
+    public function show(Category $category, Request $request, Topic $topic, User $user)
     {
         //读取分类ID关联的话题，并按每20条分页
         //$topics = Topic::where('category_id', $category->id)->paginate(20);
@@ -16,7 +17,9 @@ class CategoriesController extends Controller
             ->where('category_id', $category->id)
             ->with('user', 'category')
             ->paginate(20);
-        return view('topics.index', compact('topics', 'category'));
+        //活跃用户列表
+        $active_users = $user->getActiveUsers();
+        return view('topics.index', compact('topics', 'category', 'active_users'));
     }
 }
 
